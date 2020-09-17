b978076b3925b9d7c31e82485b0fb80c8b01fd18
Ramu <yarramsettyramu1997@gmail.com>
2020-09-17 15:39:22 +0530

    kndfk

---
+++ DB3.java
@@ -0,0 +1,15 @@
+<html>
+<head><script type="text/javaScript">
+function show_Prompt(){
+var name=prompt('enter u r name ',"shashi");
+if(name!=null && name!="")
+document.write("Hello "+"name"+" ! How is today");
+}
+</script>
+</head>
+<body>
+<script type="text/javascript">
+show_Prompt();
+</script>
+</body>
+</html>
